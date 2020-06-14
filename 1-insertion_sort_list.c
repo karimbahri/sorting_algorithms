8c62b7030d624e375e75483c523c79986194b565
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-14 13:35:53 +0000

    adding_file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,34 +2,40 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * insertion_sort_list - sort a list
+ * @list: pointer to the head of ze list
+ */
+
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *temp = NULL;
-    listint_t *reference = NULL;
+	listint_t *temp = NULL;
+	listint_t *reference = NULL;
 
-    temp = (*list)->next;
-    while(temp)
-    {
-        reference = temp;
-        while(temp->prev && (temp->n < temp->prev->n))
-        {
-            if(temp->next)
-            {
-                temp->next->prev = temp->prev;
-            }
-            temp->prev->next = temp->next;
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
-            if(temp->prev == NULL)
-            {
-                *list = temp;
-            }
-            else
-                temp->prev->next = temp;
-                print_list(*list);
-        }
-        temp = reference->next;
+	temp = (*list)->next;
+	while (temp)
+	{
+		reference = temp;
+		while (temp->prev && (temp->n < temp->prev->n))
+		{
+			if (temp->next)
+		{
+			temp->next->prev = temp->prev;
+		}
+		temp->prev->next = temp->next;
+		temp->next = temp->prev;
+		temp->prev = temp->prev->prev;
+		temp->next->prev = temp;
+		if (temp->prev == NULL)
+		{
+			*list = temp;
+		}
+		else
+			temp->prev->next = temp;
+			print_list(*list);
+		}
+		temp = reference->next;
 
-    }
+		}
 }
