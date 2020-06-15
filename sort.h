1852aeb7f37093fe73281ea6433426e836170888
mahdizaabi <mahdi.zaabi@gmail.com>
2020-06-15 01:02:21 +0000

    header_update

--- sort.h
+++ sort.h
@@ -36,4 +36,6 @@ void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_node(listint_t **listx, listint_t *tempo);
 #endif
